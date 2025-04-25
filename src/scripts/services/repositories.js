398e2f6fbbe56a2e09e1ab4ee67d6b03a6a37086
Michael Piccagli <michaelp_@hotmail.com>
2025-04-25 11:14:09 -0300

    Primeiro commit

---
+++ src/scripts/services/repositories.js
@@ -0,0 +1,8 @@
+import { baseUrl, repositoriesQantity } from "../variables.js"
+
+async function getRepositories(userName) {
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQantity}`);
+    return await response.json();
+}
+
+export { getRepositories }
---
+++ src/scripts/variables.js
@@ -0,0 +1,4 @@
+const baseUrl = "https://api.github.com/users"
+const repositoriesQantity = 10
+
+export {baseUrl, repositoriesQantity}
